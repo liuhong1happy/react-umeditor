5ff1edb9d75523453abad586bfede98f8f51969f
liuhong <liuhong@idealsee.cn>
2016-05-13 17:49:42 +0800

    [TASK]update

--- src/editor.js
+++ src/editor.js
@@ -206,6 +206,7 @@ var Editor = React.createClass({
 			case "backcolor":
 				EditorSelection.storeRange();
 				offsetPosition.y += offsetPosition.h+5;
+				
 				this.refs.color.open(offsetPosition,function(e,color){
 					editarea.focus();
 					EditorSelection.restoreRange();
@@ -237,6 +238,7 @@ var Editor = React.createClass({
 			case "formula":
 				EditorSelection.storeRange();
 				offsetPosition.y += offsetPosition.h+5;
+				offsetPosition.x -= offsetPosition.w/2;
 				this.refs.formula.open(offsetPosition,function(e,latex,id){
 					editarea.focus();
 					EditorSelection.restoreRange();
