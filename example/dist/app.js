9c0a75595fd23934f3a2baee10f3dd8e235d9faf
liuhong <liuhong@idealsee.cn>
2016-05-16 17:31:02 +0800

    [TASK]update exmaple

--- example/dist/app.js
+++ example/dist/app.js
@@ -5,11 +5,48 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Editor = require('react-umeditor');
 
+// other form control
+var FormPanel = React.createClass({
+	displayName: 'FormPanel',
+
+	getInitialState: function getInitialState() {
+		return {
+			value: this.props.value
+		};
+	},
+	componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
+		if (this.props.value != nextProps.value) {
+			this.setState({
+				value: nextProps.value
+			});
+		}
+	},
+	getValue: function getValue() {
+		return this.state.value;
+	},
+	handleChange: function handleChange(e) {
+		e = e || event;
+		var target = e.target || e.srcElement;
+		this.setState({
+			value: target.value
+		});
+	},
+	render: function render() {
+		var value = this.state.value;
+		return React.createElement('input', { type: 'text', value: value, onChange: this.handleChange });
+	}
+});
+
 var App = React.createClass({
 	displayName: 'App',
 
 	render: function render() {
-		return React.createElement(Editor, null);
+		return React.createElement(
+			'div',
+			null,
+			React.createElement(FormPanel, { value: '123' }),
+			React.createElement(Editor, null)
+		);
 	}
 });
 
--- example/src/app.js
+++ example/src/app.js
@@ -2,9 +2,42 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Editor = require('react-umeditor');
 
+// other form control
+var FormPanel = React.createClass({
+	getInitialState:function(){
+		return {
+			value:this.props.value
+		}
+	},
+	componentWillReceiveProps:function(nextProps){
+		if(this.props.value != nextProps.value){
+			this.setState({
+				value:nextProps.value
+			})
+		}
+	},
+	getValue:function(){
+		return this.state.value;
+	},
+	handleChange:function(e){
+		e = e || event;
+		var target = e.target || e.srcElement;
+		this.setState({
+			value:target.value
+		})
+	},
+	render:function(){
+		var value = this.state.value;
+		return (<input type="text" value={value} onChange={this.handleChange}/>)
+	}
+})
+
 var App = React.createClass({
 	render:function(){
-		return (<Editor />)
+		return (<div>
+				<FormPanel value="123"/>
+				<Editor />
+			   </div>)
 	}
 })
 	
